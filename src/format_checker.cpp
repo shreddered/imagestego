4bbf8c38b55c98b23399dd52b16287cb9d249470
Dmitry Kalinin <mitya17092001@gmail.com>
2020-05-05 04:53:14 +0300

    minor edits

---
+++ src/format_checker.cpp
@@ -0,0 +1,45 @@
+#include "utils/format_checker.hpp"
+
+
+template<class RAIt, typename T = typename RAIt::value_type>
+bool binarySearch(RAIt first, RAIt last, const T& data) {
+    --last;
+    while (first != last) {
+        auto m = (last - first + 1) / 2;
+        if (*(first + m) > data)
+            last = first + m - 1;
+        else
+            first = first + m;
+    }
+    return *first == data;
+}
+
+imagestego::FormatChecker::FormatChecker(int _fmt) noexcept : fmt(_fmt) {
+    switch(fmt) {
+        case Jpeg:
+            formats = std::vector<std::string>({ ".jfif",".jpeg", ".jpg" });
+            break;
+        case Png:
+            formats = std::vector<std::string>({ ".png" });
+            break;
+        case Bmp:
+            formats = std::vector<std::string>({ ".bmp", ".dib", ".rle" });
+            break;
+        case Tiff:
+            formats = std::vector<std::string>({ ".tif" });
+            break;
+        case Gif:
+            formats = std::vector<std::string>({ ".gif" });
+            break;
+    }
+}
+
+void imagestego::FormatChecker::setFormat(int _fmt) noexcept {
+    fmt = _fmt;
+}
+
+bool imagestego::FormatChecker::check(const std::string& str) {
+    auto n = str.rfind('.');
+    auto ext = str.substr(n);
+    return binarySearch(formats.begin(), formats.end(), ext); 
+}
