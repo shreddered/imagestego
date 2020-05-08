66f72299952cfbef1fbe9be8ae913b685b70bbbb
Dmitry Kalinin <mitya17092001@gmail.com>
2020-05-09 01:49:19 +0300

    lzw test passed

--- src/test/lzw.cpp
+++ src/test/lzw.cpp
@@ -1,15 +1,18 @@
 #include "imagestego/compression/lzw_encoder.hpp"
 #include "imagestego/compression/lzw_decoder.hpp"
 
+#include <fstream>
 #include <iostream>
 
 
 int main(int argc, char** argv) {
-    std::string s = "asasdasd";
+    std::ifstream in("bitarray.hpp");
+    std::ofstream out("bitarray1.hpp");
+    std::string s((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
     imagestego::LzwEncoder enc;
     enc.setMessage(s);
     auto arr = enc.getEncodedMessage();
     imagestego::LzwDecoder dec(arr);
-    std::cout << dec.getDecodedMessage() << std::endl;
+    out << dec.getDecodedMessage() << std::endl;
     return 0;
 }
