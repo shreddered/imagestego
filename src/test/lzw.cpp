36de38d83d1b386831a807696ff93d00baab3153
Dmitry Kalinin <mitya17092001@gmail.com>
2020-05-10 02:43:37 +0300

    added test

--- src/test/lzw.cpp
+++ src/test/lzw.cpp
@@ -1,18 +1,18 @@
 #include "imagestego/compression/lzw_encoder.hpp"
 #include "imagestego/compression/lzw_decoder.hpp"
 
-#include <fstream>
+#include <cassert>
 #include <iostream>
 
 
 int main(int argc, char** argv) {
-    std::ifstream in("bitarray.hpp");
-    std::ofstream out("bitarray1.hpp");
-    std::string s((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+    std::string s = "asdasdajhfahnsahsfhasghfuadhashdjahwakshdahduahs    ahsdahsdakdjajdkjj opi";
     imagestego::LzwEncoder enc;
     enc.setMessage(s);
     auto arr = enc.getEncodedMessage();
     imagestego::LzwDecoder dec(arr);
-    out << dec.getDecodedMessage() << std::endl;
+    auto s1 = dec.getDecodedMessage();
+    std::cout << dec.getDecodedMessage() << std::endl;
+    assert(s1 == s);
     return 0;
 }
