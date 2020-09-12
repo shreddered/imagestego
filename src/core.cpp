f2ffa1cd63a4ce9a60f429bd0ff8da2915c941cc
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-12 16:06:25 +0300

    new build logic

--- src/core.cpp
+++ src/core.cpp
@@ -1,4 +1,6 @@
 #include "imagestego/core.hpp"
+// third party
+#include "MurmurHash3.h"
 
 
 namespace imagestego {
@@ -37,6 +39,12 @@ uint8_t log2(unsigned int value) noexcept {
         ++res;
     return res;
 #endif
+}
+
+uint32_t hash(const std::string& _key) {
+    uint32_t tmp[1];
+    MurmurHash3_x86_32(_key.data(), _key.size(), IMAGESTEGO_MURMURHASH_SEED, tmp);
+    return tmp[0];
 }
 
 } // namespace imagestego
