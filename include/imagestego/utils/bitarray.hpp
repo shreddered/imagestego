aa098e5108b5b4d7a606349ec8004b5be73ae2f6
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-12 22:54:04 +0300

    Revert "minor edits"
    This reverts commit 897e6320cd8483fda03d62c3ce53d0760bad32ed.

--- include/imagestego/utils/bitarray.hpp
+++ include/imagestego/utils/bitarray.hpp
@@ -22,7 +22,7 @@ namespace imagestego {
 class HuffmanDecoder;
 
 template<class Block = uint16_t, class = typename std::enable_if<std::is_integral<Block>::value>::type>
-class IMAGESTEGO_EXPORTS BitArray {
+class BitArray {
     friend class HuffmanDecoder;
 private:
     class BitReference {
