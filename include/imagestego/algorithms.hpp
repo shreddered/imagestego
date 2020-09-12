6586bf728afd75d5ec89008ed98f946b36c56d80
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-13 02:49:59 +0300

    improved build config

--- include/imagestego/algorithms.hpp
+++ include/imagestego/algorithms.hpp
@@ -1,9 +1,14 @@
 #ifndef __IMAGESTEGO__ALGORITHMS_HPP_INCLUDED__
 #define __IMAGESTEGO__ALGORITHMS_HPP_INCLUDED__
 
-#include "imagestego/algorithms/dwt.hpp"
-#include "imagestego/algorithms/jpeg_lsb.hpp"
-#include "imagestego/algorithms/lsb.hpp"
-#include "imagestego/algorithms/f3.hpp"
+#ifdef IMAGESTEGO_LOSSLESS_FORMATS
+#   include "imagestego/algorithms/dwt.hpp"
+#   include "imagestego/algorithms/lsb.hpp"
+#endif /* IMAGESTEGO_LOSSLESS_FORMATS */
+
+#ifdef IMAGESTEGO_JPEG_SUPPORT
+#   include "imagestego/algorithms/jpeg_lsb.hpp"
+#   include "imagestego/algorithms/f3.hpp"
+#endif /* IMAGESTEGO_JPEG_SUPPORT */
 
 #endif /* __IMAGESTEGO__ALGORITHMS_HPP_INCLUDED__ */
