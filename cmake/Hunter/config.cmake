0bb11e4c7e119c5a1fefda4e28a06dcf034dd391
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-13 20:01:19 +0300

    edited CMakeLists.txt

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,13 +2,15 @@ hunter_config(
     Jpeg
     VERSION 9c-p0  
     CMAKE_ARGS BUILD_SHARED_LIBS=ON
+    CMAKE_BUILD_TYPE=Release
 )
 
 hunter_config(
     OpenCV
     VERSION ${HUNTER_OpenCV_VERSION}
     CMAKE_ARGS BUILD_LIST=core,imgcodecs
-    CMAKE_BUILD_TYPE=RELEASE
+    BUILD_SHARED_LIBS=ON
+    CMAKE_BUILD_TYPE=Release
     BUILD_EXAMPLES=OFF
     BUILD_TESTS=OFF
 )
