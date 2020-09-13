da774fb54202dcc6f7c671f877ba75c6feb1922c
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-13 19:39:06 +0300

    added opencv via Hunter

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -3,3 +3,12 @@ hunter_config(
     VERSION 9c-p0  
     CMAKE_ARGS BUILD_SHARED_LIBS=ON
 )
+
+hunter_config(
+    OpenCV
+    VERSION ${HUNTER_OpenCV_VERSION}
+    CMAKE_ARGS BUILD_LIST=core,imgcodecs
+    CMAKE_BUILD_TYPE=RELEASE
+    BUILD_EXAMPLES=OFF
+    BUILD_TESTS=OFF
+)
