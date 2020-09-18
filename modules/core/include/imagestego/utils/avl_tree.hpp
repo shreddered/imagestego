04850e53529ba1ec4a401c1851b16e11f7c634e6
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-18 21:37:46 +0300

    Revert "refactoring: PImpl in imagestego::Route"
    This reverts commit 4b5717c5ed28656c0f436ab5bba1bd5b8f3071fc.

--- modules/core/include/imagestego/utils/avl_tree.hpp
+++ modules/core/include/imagestego/utils/avl_tree.hpp
@@ -271,39 +271,20 @@ struct PairComparator final {
     }
 };
 
-class IMAGESTEGO_EXPORTS Route {
+class IMAGESTEGO_EXPORTS Route final : public AvlTree<std::pair<int, int>, PairComparator<int, int> > {
 private:
-    class RouteImpl final : public AvlTree<std::pair<int, int>, PairComparator<int, int> > {
-    private:
-        std::pair<int, int> _mapSize;
-        std::mt19937& gen;
-    public:
-        explicit RouteImpl(const std::pair<int, int>& mapSize, std::mt19937&) noexcept;
-        template<class It>
-        explicit RouteImpl(It begin, It end, std::mt19937& _gen) : AvlTree(begin, end), gen(_gen) {}
-        inline void setMapSize(const std::pair<int, int>& mapSize) {
-            _mapSize = mapSize;
-        }
-        void create(const int&);
-        void add();
-    }; // class RouteImpl
-    RouteImpl* route;
+    std::pair<int, int> _mapSize;
+    std::mt19937& gen;
 public:
-    typedef typename RouteImpl::iterator iterator;
-    explicit Route(const std::pair<int, int>& mapSize, std::mt19937&);
+    explicit Route(const std::pair<int, int>& mapSize, std::mt19937&) noexcept;
     template<class It>
-    explicit Route(It begin, It end, std::mt19937& gen) : route(new RouteImpl(begin, end, gen)) {}
+    explicit Route(It begin, It end, std::mt19937& _gen) : AvlTree(begin, end), gen(_gen) {}
     inline void setMapSize(const std::pair<int, int>& mapSize) {
-        route->setMapSize(mapSize);
+        _mapSize = mapSize;
     }
     void create(const int&);
     void add();
-    bool search(const std::pair<int, int>& pair);
-    iterator begin();
-    iterator end();
-    virtual ~Route() noexcept;
 }; // class Route
-
 
 } // namespace imagestego
 
--- modules/core/src/avl_tree.cpp
+++ modules/core/src/avl_tree.cpp
@@ -3,41 +3,19 @@
 
 namespace imagestego {
 
-Route::RouteImpl::RouteImpl(const std::pair<int, int>& mapSize, std::mt19937& _gen) noexcept : AvlTree(), _mapSize(mapSize), gen(_gen) {}
+Route::Route(const std::pair<int, int>& mapSize, std::mt19937& _gen) noexcept : AvlTree(), _mapSize(mapSize), gen(_gen) {}
 
-void Route::RouteImpl::create(const int& n) {
+void Route::create(const int& n) {
     while(size() != n) {
         insert({gen() % _mapSize.first, gen() % _mapSize.second});
     }
 }
 
-void Route::RouteImpl::add() {
+void Route::add() {
     auto sz = size();
     while(size() != sz + 1) {
         insert({gen() % _mapSize.first, gen() % _mapSize.second});
     }
-}
-
-Route::Route(const std::pair<int, int>& mapSize, std::mt19937& _gen) : route(new RouteImpl(mapSize, _gen))  {}
-
-void Route::create(const int& n) {
-    route->create(n);
-}
-
-void Route::add() {
-    route->add();
-}
-
-typename Route::iterator Route::begin() {
-    return route->begin();
-}
-
-typename Route::iterator Route::end() {
-    return route->end(); 
-}
-
-Route::~Route() noexcept {
-    delete route;
 }
 
 } // namespace imagestego
