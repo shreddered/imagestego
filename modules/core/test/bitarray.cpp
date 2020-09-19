c53aafda5327fe39d45dfa9a82f1ac51887af9cc
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-20 00:30:27 +0300

    edited bitarrray test

--- modules/core/test/bitarray.cpp
+++ modules/core/test/bitarray.cpp
@@ -6,13 +6,19 @@
 // gtest
 #include <gtest/gtest.h>
 
+using imagestego::BitArray;
+
 
 TEST(Core, BitArray) { 
     uint32_t temp = 53;
     char buf[4];
     memcpy(buf, &temp, sizeof(temp));
-    std::stringstream ss;
-    imagestego::BitArray arr = imagestego::BitArray::fromByteString(std::string(buf, 4));
+    std::ostringstream ss;
+    BitArray arr = BitArray::fromByteString(std::string(buf, 4));
     ss << arr;
     EXPECT_EQ(std::bitset<32>(temp).to_string(), ss.str());
+    std::ostringstream().swap(ss);
+    BitArray copy = arr;
+    ss << copy;
+    EXPECT_EQ(ss.str(), std::bitset<32>(temp).to_string());
 }
