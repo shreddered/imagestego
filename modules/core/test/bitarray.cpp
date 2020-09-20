9e12c97e88553d00f1df2686e5e82502a2647afd
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-20 13:36:28 +0300

    more BitArray tests

--- modules/core/test/bitarray.cpp
+++ modules/core/test/bitarray.cpp
@@ -13,10 +13,11 @@ TEST(Core, BitArray1) {
     uint32_t temp = 53;
     char buf[4];
     memcpy(buf, &temp, sizeof(temp));
-    std::ostringstream ss;
     BitArray arr = BitArray::fromByteString(std::string(buf, 4));
+    std::ostringstream ss;
     ss << arr;
     EXPECT_EQ(std::bitset<32>(temp).to_string(), ss.str());
+    EXPECT_EQ(ss.str(), arr.toString());
     std::ostringstream().swap(ss);
     BitArray copy = arr;
     ss << copy;
@@ -24,7 +25,19 @@ TEST(Core, BitArray1) {
 }
 
 TEST(Core, BitArray2) {
-    std::string msg = "asdasd";
+    std::string msg = "asdasdkk";
     BitArray arr = BitArray::fromByteString(msg);
     EXPECT_EQ(msg, arr.toByteString());
+    arr.clear();
+    EXPECT_TRUE(arr.empty());
+}
+
+TEST(Core, BitArray3) {
+    uint32_t temp = 53;
+    char buf[4];
+    memcpy(buf, &temp, sizeof(temp));
+    BitArray arr = BitArray::fromByteString(std::string(buf, 4));
+    std::string s = arr.toString();
+    arr.pushBack(1); s.push_back('1');
+    EXPECT_EQ(arr.toString(), s);
 }
