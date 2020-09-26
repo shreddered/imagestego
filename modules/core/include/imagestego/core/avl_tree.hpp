894461a31fe7af6e9af2bd06841ec58f496dc21f
Dmitry Kalinin <mitya17092001@gmail.com>
2020-09-26 16:29:24 +0300

    project structure fix

--- modules/core/include/imagestego/core/avl_tree.hpp
+++ modules/core/include/imagestego/core/avl_tree.hpp
@@ -291,34 +291,6 @@ struct PairComparator final {
     }
 };
 
-class RouteImpl final : public AvlTree<std::pair<int, int>, PairComparator<int, int> > {
-public:
-    explicit RouteImpl(const std::pair<int, int>& mapSize, std::mt19937&) noexcept;
-    template<class It>
-    explicit RouteImpl(It begin, It end, std::mt19937& _gen) : AvlTree(begin, end), gen(_gen) {}
-    inline void setMapSize(const std::pair<int, int>& mapSize) {
-        _mapSize = mapSize;
-    }
-    void create(const int&);
-    void add();
-private:
-    std::pair<int, int> _mapSize;
-    std::mt19937& gen;
-}; // class RouteImpl
-
-class IMAGESTEGO_EXPORTS Route {
-public:
-    explicit Route(const std::pair<int, int>& mapSize, std::mt19937& gen);
-    template<class It>
-    explicit Route(It begin, It end, std::mt19937& _gen);
-    virtual ~Route() noexcept;
-    void setMapSize(const std::pair<int, int>& mapSize);
-    void create(const int&);
-    void add();
-private:
-    RouteImpl* _route;
-}; // class Route
-
 } // namespace imagestego
 
 #endif /* __IMAGESTEGO_AVL_TREE_HPP_INCLUDED__ */
