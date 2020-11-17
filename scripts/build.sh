a6406a5628c298e554269cb54e9849b94b5695df
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-18 02:06:22 +0300

    build script enhancement

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,18 @@
 
 mkdir build || echo -n
 cd build
-cmake -D CMAKE_BUILD_TYPE=Release \
-    -D BUILD_SHARED_LIBS=ON \
-    -D IMAGESTEGO_BUILD_TESTS=ON \
-    -D IMAGESTEGO_COVERAGE=ON \
-    -D IMAGESTEGO_MODULES=core,comp,lossless ..
+if [ -z "$MODULES" ]; then
+    cmake -D CMAKE_BUILD_TYPE=Release \
+        -D BUILD_SHARED_LIBS=ON \
+        -D IMAGESTEGO_BUILD_TESTS=ON \
+        -D IMAGESTEGO_COVERAGE=ON \
+        -D IMAGESTEGO_MODULES=core,comp,lossless ..
+else
+    cmake -D CMAKE_BUILD_TYPE=Release \
+        -D BUILD_SHARED_LIBS=ON \
+        -D IMAGESTEGO_BUILD_TESTS=ON \
+        -D IMAGESTEGO_COVERAGE=ON \
+        -D IMAGESTEGO_MODULES=$MODULES ..
+fi
 make -j$(nproc)
 cd ..
