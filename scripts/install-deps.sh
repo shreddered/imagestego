2bc0be07bd3ea5348696b072d70c407e2704d9b7
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-17 22:00:46 +0300

    changed dependencies installing

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,11 +2,12 @@
 
 # Script for installing dependencies of imagestego on Travis CI
 
- git clone https://github.com/opencv/opencv.git
- cd opencv
- mkdir build
- cd build
- cmake -D BUILD_LIST=core,imgcodecs \
+if ! [ -d $TRAVIS_BUILD_DIR/opencv ]; then
+    git clone https://github.com/opencv/opencv.git
+    cd opencv
+    mkdir build
+    cd build
+    cmake -D BUILD_LIST=core,imgcodecs \
      -D CMAKE_BUILD_TYPE=Release \
      -D BUILD_EXAMPLES=OFF \
      -D BUILD_opencv_apps=OFF \
@@ -14,8 +15,11 @@
      -D BUILD_PERF_TESTS=OFF \
      -D BUILD_TESTS=OFF \
      -D CMAKE_INSTALL_PREFIX=/usr/local/ ..
- make -j$(nproc)
- sudo make install
- if [ "${TRAVIS_OS_NAME}" = "linux" ]; then sudo sh -c 'echo "/usr/local/lib/" > /etc/ld.so.conf.d/opencv.conf'; fi
- if [ "${TRAVIS_OS_NAME}" = "linux" ]; then sudo ldconfig; fi
- cd ../../
+    make -j$(nproc)
+else
+    cd $TRAVIS_BUILD_DIR/opencv/build
+fi
+sudo make install
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then sudo sh -c 'echo "/usr/local/lib/" > /etc/ld.so.conf.d/opencv.conf'; fi
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then sudo ldconfig; fi
+cd ../../
