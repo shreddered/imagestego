191c7784ceefc8efdcb861f7f9e7fdf496324080
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-17 21:44:23 +0300

    replaced sh with bash

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Script for uploading coverage report
 
--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Script for installing dependencies of imagestego on Travis CI
 
