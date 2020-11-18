3972290534aa88b8c99ea4e770b2fa31bb846563
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-18 18:39:53 +0300

    added lld on clang-10 arm64 build

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,13 +10,13 @@ if [ -z "$MODULES" ]; then
         -D BUILD_SHARED_LIBS=ON \
         -D IMAGESTEGO_BUILD_TESTS=ON \
         -D IMAGESTEGO_COVERAGE=ON \
-        -D IMAGESTEGO_MODULES=core,comp,lossless ..
+        -D IMAGESTEGO_MODULES=core,comp,lossless "$CMAKE_OPTIONS" ..
 else
     cmake -D CMAKE_BUILD_TYPE=Release \
         -D BUILD_SHARED_LIBS=ON \
         -D IMAGESTEGO_BUILD_TESTS=ON \
         -D IMAGESTEGO_COVERAGE=ON \
-        -D IMAGESTEGO_MODULES=$MODULES ..
+        -D IMAGESTEGO_MODULES=$MODULES "$CMAKE_OPTIONS" ..
 fi
 make -j$(nproc)
 cd ..
