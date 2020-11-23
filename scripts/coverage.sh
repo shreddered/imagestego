920638b649cc54e66453fca4e8c02298f1b77e87
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-23 13:46:43 +0300

    edited coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,13 +3,15 @@
 # Script for uploading coverage report
 set -e
 
-lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info \
-    '/usr/*' \
-    "$(pwd)"'/third_party/*' \
-    "$(pwd)"'/modules/core/third_party/*' \
-    "$(pwd)"'/modules/core/test/*' \
-    "$(pwd)"'/modules/compression/test/*' \
-    "$(pwd)"'/modules/lossless/test/*' \
-    --output-file coverage.info
-bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
+if [ -z "$COVERAGE" ]; then
+    lcov --directory . --capture --output-file coverage.info
+    lcov --remove coverage.info \
+        '/usr/*' \
+        "$(pwd)"'/third_party/*' \
+        "$(pwd)"'/modules/core/third_party/*' \
+        "$(pwd)"'/modules/core/test/*' \
+        "$(pwd)"'/modules/compression/test/*' \
+        "$(pwd)"'/modules/lossless/test/*' \
+        --output-file coverage.info
+    bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
+fi
