e3819c236ac291c83f688ce130cb9e1e76a8ee48
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-24 05:13:19 +0300

    added cmake checks for some compiler features

---
+++ cmake/imagestego-compiler-features.cmake
@@ -0,0 +1,28 @@
+# CMake module for detecting compiler features
+
+include(CheckIncludeFiles)
+include(CheckCXXSourceCompiles)
+
+# intrin.h header
+if (MSVC)
+  check_include_files("intrin.h" HAVE_INTRIN_H)
+endif()
+
+# inline check
+if (MSVC)
+  set(INLINE_OPTS "__inline;inline")
+else()
+  set(INLINE_OPTS "__inline__;inline")
+endif()
+foreach(inline ${INLINE_OPTS})
+  check_cxx_source_compiles("${inline} static int foo() { return 0; } int main() { return foo(); }",
+    HAVE_INLINE)
+  if (HAVE_INLINE)
+    set(INLINE ${inline})
+    break()
+  endif()
+endforeach()
+
+if (NOT HAVE_INLINE)
+  message(WARNING "Can't inline functions")
+endif()
