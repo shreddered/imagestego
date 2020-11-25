5cd231b24dd491b1802da8e60674c0c901aba04e
Dmitry Kalinin <mitya17092001@gmail.com>
2020-11-26 02:16:45 +0300

    code style edits

--- modules/wavelet/src/haar.c
+++ modules/wavelet/src/haar.c
@@ -80,15 +80,15 @@ static IMAGESTEGO_INLINE int align8(const int num) {
 
 void vertical_haar(const uint8_t* restrict _src, uint8_t* restrict _dst, const int rows,
         const int cols) {
-    static const __mmask32 len2mask[] = {0x00000000, 0x00000001, 0x00000003, 0x00000007,
-                                         0x0000000f, 0x0000001f, 0x0000003f, 0x0000007f,
-                                         0x000000ff, 0x000001ff, 0x000003ff, 0x000007ff,
-                                         0x00000fff, 0x00001fff, 0x00003fff, 0x00007fff,
-                                         0x0000ffff, 0x0001ffff, 0x0003ffff, 0x0007ffff,
-                                         0x000fffff, 0x001fffff, 0x003fffff, 0x007fffff,
-                                         0x00ffffff, 0x01ffffff, 0x03ffffff, 0x07ffffff,
-                                         0x0fffffff, 0x1fffffff, 0x3fffffff, 0x7fffffff,
-                                         0xffffffff};
+    static const __mmask32 len2mask[] = { 0x00000000, 0x00000001, 0x00000003, 0x00000007,
+                                          0x0000000f, 0x0000001f, 0x0000003f, 0x0000007f,
+                                          0x000000ff, 0x000001ff, 0x000003ff, 0x000007ff,
+                                          0x00000fff, 0x00001fff, 0x00003fff, 0x00007fff,
+                                          0x0000ffff, 0x0001ffff, 0x0003ffff, 0x0007ffff,
+                                          0x000fffff, 0x001fffff, 0x003fffff, 0x007fffff,
+                                          0x00ffffff, 0x01ffffff, 0x03ffffff, 0x07ffffff,
+                                          0x0fffffff, 0x1fffffff, 0x3fffffff, 0x7fffffff,
+                                          0xffffffff };
     int16_t* src = (int16_t*) _src;
     int16_t* dst = (int16_t*) _dst;
     for (int row = 0; row != (rows & ~1); row += 2) {
