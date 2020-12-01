6d5551c5ab08a54bfc2c0144babb9186d04786d8
Dmitry Kalinin <48997155+shreddered@users.noreply.github.com>
2020-12-01 15:14:07 +0300

    updated build script

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -4,18 +4,10 @@
 set -e
 
 mkdir -p build && cd build
-if [ -z "$MODULES" ]; then
-    cmake -D CMAKE_BUILD_TYPE=Release \
-        -D BUILD_SHARED_LIBS=ON \
-        -D IMAGESTEGO_BUILD_TESTS=ON \
-        -D IMAGESTEGO_COVERAGE=ON \
-        -D IMAGESTEGO_MODULES=core,comp,lossless ..
-else
-    cmake -D CMAKE_BUILD_TYPE=Release \
-        -D BUILD_SHARED_LIBS=ON \
-        -D IMAGESTEGO_BUILD_TESTS=ON \
-        -D IMAGESTEGO_COVERAGE=ON \
-        -D IMAGESTEGO_MODULES=$MODULES ..
-fi
+cmake -D CMAKE_BUILD_TYPE=Release \
+    -D BUILD_SHARED_LIBS=ON \
+    -D IMAGESTEGO_BUILD_TESTS=ON \
+    -D IMAGESTEGO_COVERAGE=ON \
+    -D IMAGESTEGO_MODULES=core,comp,lossless,wavelet ..
 make -j$(nproc)
 cd ..
