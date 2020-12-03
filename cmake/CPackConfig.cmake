779228ef88abdc6a9e7b694064844b197357572b
Dmitry Kalinin <x.shreddered.x@gmail.com>
2020-12-03 21:04:07 +0300

    changes source package name

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -8,6 +8,7 @@ set(CPACK_PACKAGE_VERSION_MINOR ${IMAGESTEGO_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${IMAGESTEGO_VERSION_PATCH})
 set(CPACK_PACKAGE_VERSION ${IMAGESTEGO_VERSION})
 
+# package rules
 set(CPACK_PACKAGE_DESCRIPTION "Image Steganography library written in C++ with data compression and wavelet transform support")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ Image Steganography library")
 
@@ -19,5 +20,6 @@ else()
 endif()
 
 set(CPACK_SOURCE_GENERATOR TGZ ZIP)
+set(CPACK_SOURCE_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 
 include(CPack)
